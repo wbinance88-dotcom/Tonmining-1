0276344b46f2d08f9ca32dd4e04982cf2bc9971c
wbinance88-dotcom <wbinance88@gmail.com>
2026-07-10 19:28:09 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("TON Mining");
+function showPage(page){
+
+    if(page=="home"){
+        alert("🏠 Home");
+    }
+
+    if(page=="referral"){
+        alert("👥 Referral");
+    }
+
+    if(page=="reward"){
+        alert("🎁 Daily Reward");
+    }
+
+    if(page=="task"){
+        alert("📋 Task");
+    }
+
+    if(page=="profile"){
+        alert("👤 Profile\n\nWithdraw (Minimum 0.05 TON)");
+    }
+
+}
