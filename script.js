b339dece79f3c9a4e3e35c206115e1e6b6156621
wbinance88-dotcom <wbinance88@gmail.com>
2026-07-11 18:14:41 +0630

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ console.log(db);
 let balance = Number(localStorage.getItem("balance")) || 0;
 let seconds = Number(localStorage.getItem("seconds")) || 0;
 let lastReward = Number(localStorage.getItem("lastReward")) || 0;
-localStorage.setItem("seconds", 0);
-seconds = 0;
+
 document.addEventListener("DOMContentLoaded", function () {
     updateBalance();
     updateTimer();
